601bb8602fa5b61b9faab07749f221796d48575b
Omar Mahmood <36893410+Omar717@users.noreply.github.com>
2026-04-04 19:13:09 +0100

    Make performance enhancements
    Improve performance by changing how events are handled

--- javascript.js
+++ javascript.js
@@ -113,44 +113,150 @@ function changeDisplay (input) {
     } 
 }
 
-const numButtons = document.querySelector("#num-buttons");
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
 
-numButtons.addEventListener("click", (e) => {
-    let target = e.target;
-    if (!target.matches(".buttons")) {
-        return;
-    }
-    if (target.textContent !== ".") {
-        changeDisplay(target.textContent);
-        getNum(target.textContent);
-    } else if (target.textContent === ".") {
-        if (!state.display.includes(".") && !state.display.includes("e")) {
-            state.display += target.textContent;
-            display.textContent += ".";
-        }
-    } else if (target.textContent === "0") {
-        if (state.numOne !== "") {
-            state.display += target.textContent;
+const decimal = document.querySelector("#decimal");
+const AC = document.querySelector("#clear");
+
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const multiplication = document.querySelector("#multiply");
+const division = document.querySelector("#divide");
+
+const equals = document.querySelector("#equals");
+
+zero.addEventListener("click", () => {
+    if (state.numOne !== "") {
+            state.display += "0";
             display.textContent += "0";
         }
-    }
-    if (target.textContent === "AC") {
-        clear();
-    }    
 });
 
-const operateButtons = document.querySelector("#operator-buttons");
-
-operateButtons.addEventListener("click", (e) => {
-    let target = e.target;
-    if (!target.matches(".op-buttons")) {
-        return;
-    }
-    getOperator(target.textContent)
+one.addEventListener("click", () => {
+    changeDisplay("1");
+        getNum("1");
 });
 
-const equalsButton = document.querySelector("#equals");
-
-equalsButton.addEventListener("click", () => {
-        evaluate();
+two.addEventListener("click", () => {
+    changeDisplay("2");
+        getNum("2");
 });
+
+three.addEventListener("click", () => {
+    changeDisplay("3");
+        getNum("3");
+});
+
+four.addEventListener("click", () => {
+    changeDisplay("4");
+        getNum("4");
+});
+
+five.addEventListener("click", () => {
+    changeDisplay("5");
+        getNum("5");
+});
+
+six.addEventListener("click", () => {
+    changeDisplay("6");
+        getNum("6");
+});
+
+seven.addEventListener("click", () => {
+    changeDisplay("7");
+        getNum("7");
+});
+
+eight.addEventListener("click", () => {
+    changeDisplay("8");
+        getNum("8");
+});
+
+nine.addEventListener("click", () => {
+    changeDisplay("9");
+        getNum("9");
+});
+
+decimal.addEventListener("click", () => {
+    if (!state.display.includes(".") && !state.display.includes("e")) {
+            state.display += ".";
+            display.textContent += ".";
+        }
+});
+
+AC.addEventListener("click", () => {
+    clear();
+});
+
+plus.addEventListener("click", () => {
+    getOperator("+");
+});
+
+minus.addEventListener("click", () => {
+    getOperator("-");
+});
+
+multiplication.addEventListener("click", () => {
+    getOperator("*");
+});
+
+division.addEventListener("click", () => {
+    getOperator("/");
+});
+
+equals.addEventListener("click", () => {
+    evaluate();
+})
+
+// Event Delegation Attempt:
+
+// const numButtons = document.querySelector("#num-buttons");
+
+// numButtons.addEventListener("click", (e) => {
+//     let target = e.target;
+//     if (!target.matches(".buttons")) {
+//         return;
+//     }
+//     if (target.textContent !== ".") {
+//         changeDisplay(target.textContent);
+//         getNum(target.textContent);
+//     } else if (target.textContent === ".") {
+//         if (!state.display.includes(".") && !state.display.includes("e")) {
+//             state.display += target.textContent;
+//             display.textContent += ".";
+//         }
+//     } else if (target.textContent === "0") {
+//         if (state.numOne !== "") {
+//             state.display += target.textContent;
+//             display.textContent += "0";
+//         }
+//     }
+//     if (target.textContent === "AC") {
+//         clear();
+//     }    
+// });
+
+// const operateButtons = document.querySelector("#operator-buttons");
+
+// operateButtons.addEventListener("click", (e) => {
+//     let target = e.target;
+//     if (!target.matches(".op-buttons")) {
+//         return;
+//     }
+//     getOperator(target.textContent)
+// });
+
+// const equalsButton = document.querySelector("#equals");
+
+// equalsButton.addEventListener("click", () => {
+//         evaluate();
+// });
